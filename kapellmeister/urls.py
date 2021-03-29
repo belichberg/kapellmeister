dd4a193d22eca579b99ac6e49b1afb81509a5d31
Ilya Redkolis <ir@belichberg.com>
2021-03-29 15:04:53 +0300

    change static settings

--- kapellmeister/urls.py
+++ kapellmeister/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+
+from kapellmeister import settings
 from management.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index),
     path("api/v1/", include("management.urls")),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
