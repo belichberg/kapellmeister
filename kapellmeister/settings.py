c622922c286cdc3e4662ad4ae01862c98f398384
Ilya Redkolis <ir@belichberg.com>
2021-03-29 17:43:23 +0300

    change static settings

--- kapellmeister/settings.py
+++ kapellmeister/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.gzip.GZipMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -122,3 +123,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- kapellmeister/urls.py
+++ kapellmeister/urls.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.static import serve
-
-from kapellmeister import settings
+from django.urls import path, include
 from management.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index),
     path("api/v1/", include("management.urls")),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
