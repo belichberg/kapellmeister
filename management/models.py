9656e4439c35db4a9d583d2cd5c86af163151004
Ilya Redkolis <ir@belichberg.com>
2021-04-07 11:43:04 +0300

    change models

--- management/models.py
+++ management/models.py
@@ -14,7 +14,7 @@ class Project(models.Model):
 class Channel(models.Model):
 
     name = models.CharField(max_length=64)
-    slug = models.SlugField(max_length=64, unique=True)
+    slug = models.SlugField(max_length=64)
     description = models.TextField(max_length=512, null=True, blank=True)
     project = models.ForeignKey(
         Project,
@@ -24,14 +24,19 @@ class Channel(models.Model):
         on_delete=models.CASCADE,
     )
 
+    class Meta:
+        ordering = ["project"]
+        unique_together = [
+            ("slug", "project"),
+        ]
+
     def __str__(self):
         return self.name
 
 
 class Container(models.Model):
 
-    slug = models.SlugField(max_length=128, unique=True)
-    # image_path = models.URLField(max_length=512, null=True, blank=True)
+    slug = models.SlugField(max_length=128)
     auth = models.CharField(max_length=2000, null=True, blank=True)
     hash = models.CharField(max_length=255)
     parameters = models.CharField(max_length=2000)
