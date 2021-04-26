89116f7cacf6e7095522a436fe77bbc2c41740f8
Alexander Nechipurenko <an@bel.net.ua>
2021-04-26 10:31:36 +0300

    Added docker-entrypoint.sh and user create

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,4 @@ WORKDIR /app
 COPY requirements.txt /app/
 RUN pip install -r requirements.txt
 COPY . /app/
-
-CMD [ "python", "manage.py", "runserver", "0.0.0.0:8000" ]
+ENTRYPOINT ["/app/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'admin')" | python manage.py shell
+python manage.py runserver 0.0.0.0:8000
