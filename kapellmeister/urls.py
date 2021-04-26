8c82f44617b1465eda21acf21d99c078ef64842d
Ilya Redkolis <ir@belichberg.com>
2021-04-26 09:57:30 +0300

    change urls

--- kapellmeister/urls.py
+++ kapellmeister/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from management.views import index
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", index),
+    path("", admin.site.urls),
+    # path("", index),
     path("api/v1/", include("management.urls")),
 ]
