5994e997f6035c9c4bc704a3df9066c1f6081f41
Alex Shu <alexandra.velikaya@yahoo.com>
2021-05-19 17:04:31 +0300

    frontend login form

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from envyaml import EnvYAML
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, Depends
 import sentry_sdk
 from fastapi.responses import PlainTextResponse
+
+from src.database.models import Container
+from src.helpers import get_db
 from src.routers import manager
 # from src.database.db import database
 from src.database.db import SessionLocal
+from sqlalchemy.orm import Session
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 # read env.yaml config file
 env = EnvYAML()
 
 DEBUG: bool = env.get("DEBUG", False)
 API_ROUTE_PREFIX: str = "/api/v1"
+
 
 # Fast api start
 app = FastAPI(
@@ -21,6 +28,12 @@ app = FastAPI(
     # docs_url="/docs" if DEBUG else None,
     docs_url="/docs",
 )
+
+# add static files to project
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# add templates to project
+templates = Jinja2Templates(directory="templates")
 
 # Sentry integration
 if not env.get("DEBUG"):
@@ -50,8 +63,19 @@ async def db_session_middleware(request: Request, call_next):
 
 
 @app.get("/")
-async def root():
+async def root(db: Session = Depends(get_db)):
+    # print(db.query(Container).first().__dict__)
     return PlainTextResponse("Kapellmeister")
+
+@app.get("/login")
+async def home(request: Request):
+    """Create user login page"""
+    return templates.TemplateResponse(
+            "login.html",
+            {
+                "request": request
+            }
+    )
 
 # include routes
 app.include_router(manager.router, prefix=API_ROUTE_PREFIX)
