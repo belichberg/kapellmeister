0e88112fe6d5b70fb3181333b7209057d0785f5f
Alex Shu <alexandra.velikaya@yahoo.com>
2021-06-11 15:11:19 +0300

    code refactoring

--- src/routers/token.py
+++ src/routers/token.py
@@ -1,15 +1,9 @@
-from datetime import timedelta
-from typing import Optional
+from fastapi import APIRouter
 
-from fastapi import APIRouter, Depends, HTTPException, status, Request
-from fastapi.responses import RedirectResponse
-from fastapi.security import OAuth2PasswordRequestForm
-from fastapi.templating import Jinja2Templates
+from fastapi import APIRouter
 
-from src.database.models import User, APIToken
-from src.dependencies import time_utc_now, pwd_verify, JWT_TOKEN_EXPIRE, token_create, JWT_KEY, JWT_ALGORITHM
+from src.database.models import APIToken
 from src.models.manager import TokenAPI
-from src.models.user import UserAPI, TokenData, JWTToken
 
 router = APIRouter()
 
